bd553eccae826daf847b546c0772808da39d7dce
Regine Mae Hambiol <163650173+diareinxs@users.noreply.github.com>
2024-03-25 15:02:43 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,6 @@
 //Create a calculator using User Input:
 /* Should be accepting numerical values, otherwise, display invalid. When the scenario occurs, terminate the App
 user may input(int, double, float, etc.), instruct the user to input any of the 4 fundamental operators: +,-,/, and *  otherwise display invalid message:/*
-Input valid prompt the user to input another numerical value. Display an error message again when invalid.
-When valid display the results:
-When the results in displayed, ask the user if he wants to do another calculation again: If Yes, procede with the First Step otherwise No end the vode.
+//Input valid prompt the user to input another numerical value. Display an error message again when invalid.
+//When valid display the results:
+//When the results in displayed, ask the user if he wants to do another calculation again: If Yes, procede with the First Step otherwise No end the vode.
