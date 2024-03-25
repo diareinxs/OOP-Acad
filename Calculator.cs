3cdaab716cbf52e2187693b99968fc791427407f
Regine Mae Hambiol <163650173+diareinxs@users.noreply.github.com>
2024-03-25 07:31:04 +0000

    modified: Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,9 @@
 //Check list for Calculator Activity in C#:
 //Create a calculator using User Input:
 /* Should be accepting numerical values, otherwise, display invalid. When the scenario occurs, terminate the App
-user may input(int, double, float, etc.), instruct the user to input any of the 4 fundamental operators: +,-,/, and *  otherwise display invalid message:/*
+user may input(int, double, float, etc.), 
+//instruct the user to input any of the 4 fundamental operators: +,-,/, and *  otherwise display invalid message:/*
 //Input valid prompt the user to input another numerical value. Display an error message again when invalid.
 //When valid display the results:
-//When the results in displayed, ask the user if he wants to do another calculation again: If Yes, procede with the First Step otherwise No end the vode.
+//When the results in displayed, ask the user if he wants to do another calculation again: 
+//If Yes, procede with the First Step otherwise No end the vode.
